1451d4345b9f387f358e0243bd03dbc3ade584f6
Irenevn16 <irenevn16@gmail.com>
2025-05-26 18:55:31 +0200

    test mockmvc

--- src/main/java/traveler_service/traveler_service/controllers/TravelerController.java
+++ src/main/java/traveler_service/traveler_service/controllers/TravelerController.java
@@ -33,6 +33,7 @@ public class TravelerController {
             Traveler foundTraveler = travelerService.findTravelerById(id);
 
             TrainDTO foundTrain = trainFeignClient.getTrainById(foundTraveler.getTrainId());
+            System.out.println(foundTraveler);
             System.out.println(foundTrain);
 
             Map<String, Object> response = new HashMap<>();
