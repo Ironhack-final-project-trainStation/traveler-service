ae6f17f9bfee606a087ec1f55b4c597d8afe38e9
Irenevn16 <irenevn16@gmail.com>
2025-05-26 10:16:47 +0200

    add model traveler

--- src/main/java/traveler_service/traveler_service/TravelerServiceApplication.java
+++ src/main/java/traveler_service/traveler_service/TravelerServiceApplication.java
@@ -2,8 +2,10 @@ package traveler_service.traveler_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TravelerServiceApplication {
 
 	public static void main(String[] args) {
