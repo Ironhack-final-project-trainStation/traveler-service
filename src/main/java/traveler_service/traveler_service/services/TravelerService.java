04e570b704905c697f88013ad33438f38e3a738e
Irenevn16 <irenevn16@gmail.com>
2025-05-26 10:24:09 +0200

    add service

--- src/main/java/traveler_service/traveler_service/services/TravelerService.java
+++ src/main/java/traveler_service/traveler_service/services/TravelerService.java
@@ -6,6 +6,8 @@ import traveler_service.traveler_service.exceptions.TravelerNotFoundException;
 import traveler_service.traveler_service.models.Traveler;
 import traveler_service.traveler_service.repositories.TravelerRepository;
 
+import java.util.Optional;
+
 @Service
 public class TravelerService {
 
@@ -13,6 +15,12 @@ public class TravelerService {
     TravelerRepository travelerRepository;
 
     public Traveler findTravelerById(Long id) throws TravelerNotFoundException {
+        Optional<Traveler> foundTraveler = travelerRepository.findById(id);
 
+        if (foundTraveler.isPresent()) {
+            return foundTraveler.get();
+        } else {
+            throw new TravelerNotFoundException("Traveler not found");
+        }
     }
 }
