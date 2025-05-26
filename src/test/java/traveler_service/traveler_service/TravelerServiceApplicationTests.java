ea310fee35c119af596421ac4b02f36c0ce757a0
Irenevn16 <irenevn16@gmail.com>
2025-05-26 09:50:38 +0200

    initial commit

---
+++ src/test/java/traveler_service/traveler_service/TravelerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package traveler_service.traveler_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
